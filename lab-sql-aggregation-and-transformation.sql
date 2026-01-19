600b3b5f1fd3a9ce95769cf0076eb42fb4df26b6
Ako <ako.kitamura2211@gmail.com>
2026-01-19 13:42:36 +0100

    lab re-submitted

--- lab-sql-aggregation-and-transformation.sql
+++ lab-sql-aggregation-and-transformation.sql
@@ -12,9 +12,23 @@ FROM sakila.rental;
 SELECT title, IFNULL(rental_duration, 'NOT Available') as Rental_Duration FROM sakila.film ORDER BY rental_duration ASC;
 
 -- Challenge 2, 1.1-1.3 
+-- 1.1
+SELECT COUNT(*) AS total_films FROM sakila.film;
+
+-- 1.2 
+SELECT rating, COUNT(*) AS number_of_films FROM sakila.film GROUP BY rating;
+
+-- 1.3 
 SELECT rating, count(rating) as count FROM sakila.film GROUP BY rating ORDER BY RIGHT(rating,2) DESC; 
+
 -- Challenge 2, 2.1-2.3
+-- 2.1 
+SELECT rating, ROUND(AVG(length), 2) AS mean_duration FROM sakila.film GROUP BY rating ORDER BY mean_duration DESC;
+
+-- 2.2 
+SELECT rating, ROUND(AVG(length), 2) AS mean_duration FROM sakila.film GROUP BY rating HAVING mean_duration > 120;
+
+-- 2.3 
 SELECT ROUND(AVG(length),2) as length, rating FROM sakila.film GROUP BY rating ORDER BY rating DESC; 
-SELECT rating, title FROM sakila.film WHERE length >120 ; 
 
 
